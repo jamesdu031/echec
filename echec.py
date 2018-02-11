d72c3cc7b8b3071df2ec60033c48a9da09d29c6c
jamesdu031 <35631276+jamesdu031@users.noreply.github.com>
2018-02-11 14:41:15 +0100

    First commit
    Fichier echec.py et image de l'échiquier

---
+++ echec.py
@@ -0,0 +1,20 @@
+﻿# Créé par charlier, le 25/01/2018 en Python 3.2
+from tkinter import *
+import PIL.Image as Im
+
+def click(event):
+    print(event.x//92, event.y//86)
+
+fenetre = Tk()
+photo = PhotoImage(file="echiquier.gif", format="gif")
+canvas = Canvas(fenetre,width=735, height=689)
+canvas.create_image(0, 0, anchor=NW, image=photo)
+piece = canvas.create_oval(0,0,92,86,fill="blue")
+canvas.pack()
+canvas.bind("<Button-1>", click)
+fenetre.mainloop()
+
+
+
+
+
