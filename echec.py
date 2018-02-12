775bbd0d3533de341f45280d6c28b2e87f114ffe
jamesdu031 <35631276+jamesdu031@users.noreply.github.com>
2018-02-12 10:54:41 +0100

    Second commit

--- echec.py
+++ echec.py
@@ -1,18 +1,46 @@
 ﻿# Créé par charlier, le 25/01/2018 en Python 3.2
 from tkinter import *
+from time import sleep
 import PIL.Image as Im
 
-def click(event):
-    print(event.x//92, event.y//86)
+
+
+def clickx(event):
+    return event.x
+
+def clicky(event):
+    return event.y
+
+def oval(x,y):
+    """affiche un cercle de coordonnéés x et y"""
+    piece = canvas.create_oval(x,y,x+92,y+92,fill="blue")
+    return piece
+
+def souris(event):
+    global coords
+    if x<=clickx<=x+92:
+        canvas.coords(pieceout, 100, 100, 100+92, 100+92)
+
+
 
 fenetre = Tk()
-photo = PhotoImage(file="echiquier.gif", format="gif")
-canvas = Canvas(fenetre,width=735, height=689)
+photo = PhotoImage(file="echiquier2.gif", format="gif")
+canvas = Canvas(fenetre,width=800, height=800)
 canvas.create_image(0, 0, anchor=NW, image=photo)
-piece = canvas.create_oval(0,0,92,86,fill="blue")
+
 canvas.pack()
-canvas.bind("<Button-1>", click)
+canvas.bind("<Button-1>", clickx)
+
+
+pieceout = oval(200,0)
+canvas.focus_set()
+canvas.bind("<Button-1>", souris)
 fenetre.mainloop()
+
+
+
+
+
 
 
 
