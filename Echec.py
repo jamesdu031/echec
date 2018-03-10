3ba622a818caac5ca65e122d75f6067ded0e53b2
jamesdu031 <35631276+jamesdu031@users.noreply.github.com>
2018-03-10 10:45:32 +0100

    Second commit
    Rajout par mes collègues de certaines pièces de l'échiquier comme le fou, un pion et la tour

--- Echec.py
+++ Echec.py
@@ -2,20 +2,19 @@ import pygame
 from pygame.locals import *
 import time
 
-
 BLACK = (150,   150,   150  )
 WHITE  = (255,   255,   255)
 
-#constants representing the different resources
+#Variables des couleurs
 B  = 0
 W = 1
-#a dictionary linking resources to colours
+#Creation d'un dictionnaire pour les couleurs utilises
 colours =   {
                 B  : BLACK,
                 W : WHITE,
             }
 
-#a list representing our tilemap
+#Une liste qui represente l'echiquier
 tilemap = [
             [B,W,B,W,B,W,B,W],
             [W,B,W,B,W,B,W,B],
@@ -27,7 +26,7 @@ tilemap = [
             [W,B,W,B,W,B,W,B]
           ]
 
-#useful game dimensions
+#Dimension de l'echiquier
 TILESIZE  = 100
 MAPWIDTH  = 8
 MAPHEIGHT = 8
@@ -36,19 +35,47 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
 reine = []
 tour = []
-#Ouverture de la fen?tre Pygame
+#Ouverture de la fenetre Pygame
 fenetre = pygame.display.set_mode((800, 800))
 pygame.display.set_caption('Echec')
 #Chargement et collage du fond
 
-#Chargement et collage des pi?ces
+#Chargement et collage des pieces
 reinep = pygame.image.load("pion/reine.png").convert_alpha()
 reine = [0,0]
 fenetre.blit(reinep, (reine[0], reine[1]))
 
+roip = pygame.image.load("pion/roi.png").convert_alpha()
+roi = [0,0]
+fenetre.blit(roip, (roi[0], roi[1]))
+
 tourp = pygame.image.load("pion/tour.png").convert_alpha()
 tour = [0,0]
 fenetre.blit(tourp, (tour[0], tour[1]))
+
+chavalierp = pygame.image.load("pion/chavalier.png").convert_alpha()
+chavalier = [0,0]
+fenetre.blit(chavalierp, (chavalier[0], chavalier[1]))
+
+foup = pygame.image.load("pion/fou.png").convert_alpha()
+fou = [0,0]
+fenetre.blit(foup, (fou[0], fou[1]))
+
+tourp2 = pygame.image.load("pion/tour.png").convert_alpha()
+tour2 = [0,0]
+fenetre.blit(tourp2, (tour2[0], tour2[1]))
+
+chavalierp2 = pygame.image.load("pion/chavalier.png").convert_alpha()
+chavalier2 = [0,0]
+fenetre.blit(chavalierp2, (chavalier2[0], chavalier2[1]))
+
+foup2 = pygame.image.load("pion/fou.png").convert_alpha()
+fou2 = [0,0]
+fenetre.blit(foup2, (fou2[0], fou2[1]))
+
+pionp = pygame.image.load("pion/pion.png").convert_alpha()
+pion = [0,0]
+fenetre.blit(pionp, (pion[0], pion[1]))
 
 def pos_piece(piece, abscisse, ordonnee):
     if abscisse <= 7 or ordonnee <= 7:
@@ -59,9 +86,28 @@ def pos_piece(piece, abscisse, ordonnee):
         piece[0] = 0
         piece[1] = 0
 
+def deplacement(piece, piecepo):
+    global event
+    if piecepo.collidepoint(event.pos[0],event.pos[1]):         #Prend le pion
+        print("Select G")
+        time.sleep(2)                                          #Attend 2s avant annulation
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:                  #Pose le pion
+                if event.button == 1:
+                    print("Select D")
+                    pos_piece(piece,event.pos[0] // 100,event.pos[1] // 100)
+        print("ok piece")
 
-pos_piece(tour, 0, 0)
 pos_piece(reine, 3, 0)
+pos_piece(roi, 4, 0)
+pos_piece(chavalier, 1, 0)
+pos_piece(fou, 2, 0)
+pos_piece(tour, 0, 0)
+pos_piece(chavalier2, 6, 0)
+pos_piece(fou2, 5, 0)
+pos_piece(tour2, 7, 0)
+pos_piece(pion, 0, 1)
+
 
 pygame.display.flip()
 
@@ -73,33 +119,32 @@ while (continuer):
                     continuer = False
 		if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
-                            if reinepo.collidepoint(event.pos[0],event.pos[1]):          #Prend le pion
-                                print("Select G")
-                                time.sleep(2)                                           #Attend 2s avant annulation
-                                for event in pygame.event.get():
-                                    if event.type == MOUSEBUTTONDOWN:                  #Pose le pion
-                                            if event.button == 1:
-                                                print("Select D")
-                                                pos_piece(reine,event.pos[0] // 100,event.pos[1] // 100)
-                                print("ok reine")
-                            if tourpo.collidepoint(event.pos[0],event.pos[1]):         #Prend le pion
-                                print("Select G")
-                                time.sleep(2)                                          #Attend 2s avant annulation
-                                for event in pygame.event.get():
-                                    if event.type == MOUSEBUTTONDOWN:                  #Pose le pion
-                                            if event.button == 1:
-                                                print("Select D")
-                                                pos_piece(tour,event.pos[0] // 100,event.pos[1] // 100)
-                                print("ok tour")
+                            deplacement(reine, reinepo)
+                            deplacement(roi, roipo)
+                            deplacement(tour, tourpo)
+                            deplacement(chavalier, chavalierpo)
+                            deplacement(fou, foupo)
+                            deplacement(tour2, tourpo2)
+                            deplacement(chavalier2, chavalierpo2)
+                            deplacement(fou2, foupo2)
+
+                            deplacement(pion, pionpo)
         for row in range(MAPHEIGHT):
-                 #loop through each column in the row
+                 #Creation de l'echiquier
                 for column in range(MAPWIDTH):
-                    #draw the resource at that position in the tilemap, using the correct colour
+                    #Dessine les cases a la bonne place et en utilisant le bonnes couleurs (noirs et blancs)
                     pygame.draw.rect(DISPLAYSURF, colours[tilemap[row][column]], (column*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
 
 	#Re-collage
 	reinepo = fenetre.blit(reinep, (reine[0], reine[1]))
 	tourpo = fenetre.blit(tourp, (tour[0], tour[1]))
-	#Rafraichissement
+        chavalierpo = fenetre.blit(chavalierp, (chavalier[0], chavalier[1]))
+        foupo = fenetre.blit(foup, (fou[0], fou[1]))
+        roipo = fenetre.blit(roip, (roi[0], roi[1]))
+        pionpo = fenetre.blit(pionp, (pion[0], pion[1]))
+        chavalierpo2 = fenetre.blit(chavalierp2, (chavalier2[0], chavalier2[1]))
+        tourpo2 = fenetre.blit(tourp2, (tour2[0], tour2[1]))
+        foupo2 = fenetre.blit(foup2, (fou2[0], fou2[1]))
+    #Rafraichissement
 	pygame.display.flip()
 pygame.quit()
